a79d947e1260cacfbb4f3788d810433d0c0a763b
Shri <shrinidhi.hegde@equentis.com>
2023-05-15 18:20:44 +0530

    chore: cleaned up code and reorganised project structure

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -4,8 +4,8 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from src.database_models.models import Base
 from src.env_config.env import env_variables
-from src.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
